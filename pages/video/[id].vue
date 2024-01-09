8119a9998f77984660709078b64ab997d2408bb3
hsmtkk <hsmtkk@gmail.com>
2024-01-10 07:59:24 +0900

    implemented video detail page

--- pages/video/[id].vue
+++ pages/video/[id].vue
@@ -1,1 +1,30 @@
-<template></template>
+<template>
+    <v-container>
+        <iframe id="ytplayer" type="text/html" width="640" height="360" v-bind:src="youtubeURL" frameborder="0"></iframe>
+        <v-list>
+            <v-list-item>{{ data.snippet.title }}</v-list-item>
+            <v-list-item>{{ data.snippet.channelTitle }}</v-list-item>
+            <v-list-item>{{ data.snippet.publishedAt }}</v-list-item>
+            <v-list-item>{{ shortDescription }} ...</v-list-item>
+        </v-list>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const id = route.params.id
+
+const { data } = await useFetch(`/api/video/${id}`)
+
+const shortDescription = computed(() => {
+    return data.value.snippet.description.slice(0, 140)
+})
+
+const youtubeURL = computed(() => {
+    return `https://www.youtube.com/embed/${id}`
+})
+
+// console.log("data")
+// console.log(data)
+</script>
--- server/api/video/[id].get.ts
+++ server/api/video/[id].get.ts
@@ -1,5 +1,20 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
     console.log("/api/video/:id")
-    const id = event.context.params.id
-    return { message: "dummy" }
+    const query = getQuery(event)
+    const id = query.id
+
+    const config = useRuntimeConfig()
+    const apiKey = config.YOUTUBE_API_KEY
+
+    const resp = await $fetch("https://www.googleapis.com/youtube/v3/videos", {
+        query: {
+            key: apiKey,
+            part: "id,snippet",
+            chart: "mostPopular",
+            id: id,
+        },
+    })
+    // console.log("resp")
+    // console.log(resp)
+    return resp.items[0]
 })
